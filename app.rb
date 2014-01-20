21a9b4b801f0f22e037337f9f203e7a36d7819fe
Raphael Sofaer <rafi@sofaer.net>
2014-01-20 10:39:13 -0800

    CRUD for products

---
+++ app.rb
@@ -0,0 +1,95 @@
+require 'pry'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'pg'
+
+dbname = "storeadminsite"
+def with_db
+  c = PGconn.new(:host => "localhost", :dbname => dbname)
+  yield c
+  c.close
+end
+
+get '/' do
+  erb :index
+end
+
+# The Products machinery:
+
+# Get the index of products
+get '/products' do
+  c = PGconn.new(:host => "localhost", :dbname => dbname)
+  @products = c.exec_params("SELECT * FROM products;")
+  c.close
+  erb :products
+end
+
+# Get the form for creating a new product
+get '/products/new' do
+  erb :new_product
+end
+
+# POST to create a new product
+post '/products' do
+  c = PGconn.new(:host => "localhost", :dbname => dbname)
+  c.exec_params("INSERT INTO products (name, price, description) VALUES ($1,$2,$3)",
+                  [params["name"], params["price"], params["description"]])
+
+  # Assuming you created your products table with "id SERIAL PRIMARY KEY",
+  # This will get the id of the product you just created.
+  new_product_id = c.exec_params("SELECT currval('products_id_seq');").first["currval"]
+  c.close
+  redirect "/products/#{new_product_id}"
+end
+
+# Update a product
+post '/products/:id' do
+  c = PGconn.new(:host => "localhost", :dbname => dbname)
+  product = c.exec_params("SELECT * FROM products WHERE products.id = $1", [params["id"]])
+  c.exec_params("UPDATE products SET (name, price, description) = ($2, $3, $4) WHERE products.id = $1 ",
+                [params["id"], params["name"], params["price"], params["description"]])
+  c.close
+  redirect "/products/#{params["id"]}"
+end
+
+get '/products/:id/edit' do
+  c = PGconn.new(:host => "localhost", :dbname => dbname)
+  @product = c.exec_params("SELECT * FROM products WHERE products.id = $1", [params["id"]]).first
+  c.close
+  erb :edit_product
+end
+# DELETE to delete a product
+post '/products/:id/destroy' do
+
+  c = PGconn.new(:host => "localhost", :dbname => dbname)
+  c.exec_params("DELETE FROM products WHERE products.id = $1", [params["id"]])
+  c.close
+  redirect '/products'
+end
+
+# GET the show page for a particular product
+get '/products/:id' do
+  c = PGconn.new(:host => "localhost", :dbname => dbname)
+  @product = c.exec_params("SELECT * FROM products WHERE products.id = $1;", [params[:id]]).first
+  c.close
+  erb :product
+end
+
+def create_products_table
+  c = PGconn.new(:host => "localhost", :dbname => dbname)
+  c.exec %q{
+  CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name varchar(255),
+    price decimal,
+    description text
+  );
+  }
+  c.close
+end
+
+def drop_products_table
+  c = PGconn.new(:host => "localhost", :dbname => dbname)
+  c.exec "DROP TABLE products;"
+  c.close
+end
