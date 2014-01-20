f9e6cf7fe14b80a577c99455c106beaf43437465
Raphael Sofaer <rafi@sofaer.net>
2014-01-20 10:44:06 -0800

    Lab Solution

--- app.rb
+++ app.rb
@@ -63,6 +63,15 @@ end
 get '/products/:id/edit' do
   c = PGconn.new(:host => "localhost", :dbname => dbname)
   @product = c.exec_params("SELECT * FROM products WHERE products.id = $1", [params["id"]]).first
+  @member_categories = c.exec_params("SELECT category_memberships.id AS cm_id, categories.name AS cat_name FROM products
+        INNER JOIN category_memberships ON category_memberships.product_id=products.id
+        INNER JOIN categories ON category_memberships.category_id=categories.id
+        WHERE products.id=$1;", [params["id"]])
+  @non_member_categories = c.exec_params("SELECT categories.* FROM categories
+        LEFT OUTER JOIN category_memberships ON
+          category_memberships.category_id=categories.id
+          AND category_memberships.product_id=$1
+        WHERE category_memberships.id IS NULL;", [params["id"]])
   c.close
   erb :edit_product
 end
@@ -79,8 +88,98 @@ end
 get '/products/:id' do
   c = PGconn.new(:host => "localhost", :dbname => dbname)
   @product = c.exec_params("SELECT * FROM products WHERE products.id = $1;", [params[:id]]).first
+  @categories = c.exec_params("SELECT categories.id, categories.name FROM products
+        INNER JOIN category_memberships ON category_memberships.product_id=products.id
+        INNER JOIN categories ON category_memberships.category_id=categories.id
+        WHERE products.id=$1;", [params["id"]])
   c.close
   erb :product
+end
+
+# Categories
+
+# GET the index of all categories:
+get '/categories' do
+  c = PGconn.new(:host => "localhost", :dbname => dbname)
+  @categories = c.exec_params("SELECT * FROM categories")
+  c.close
+  erb :categories
+end
+
+# Form to create a new category:
+get '/categories/new' do
+  erb :new_category
+end
+
+# Show page for a category:
+get '/categories/:id' do
+  c = PGconn.new(:host => "localhost", :dbname => dbname)
+  @category = c.exec_params("SELECT * FROM categories WHERE categories.id=$1", [params["id"]]).first
+  @products = c.exec_params("SELECT products.* FROM products
+                             INNER JOIN category_memberships
+                               ON category_memberships.product_id=products.id
+                             INNER JOIN categories
+                               ON category_memberships.category_id=categories.id
+                             WHERE categories.id=$1;", [params["id"]])
+  c.close
+  erb :category
+end
+
+# POST to create a new category
+post '/categories' do
+  c = PGconn.new(:host => "localhost", :dbname => dbname)
+  c.exec_params("INSERT INTO categories (name) VALUES ($1)",
+                  [params["name"]])
+
+  # Assuming you created your categories table with "id SERIAL PRIMARY KEY",
+  # This will get the id of the category you just created.
+  # id SERIAL automatically creates a sequence called table-name_column-name_seq.
+  new_category_id = c.exec_params("SELECT currval('categories_id_seq');").first["currval"]
+  c.close
+  redirect "/categories/#{new_category_id}"
+end
+
+# POST to add a product to a category
+post '/category_memberships' do
+  c = PGconn.new(:host => "localhost", :dbname => dbname)
+  c.exec_params("INSERT INTO category_memberships (category_id, product_id) VALUES($1, $2)",
+                 [params["category_id"], params["product_id"]])
+  c.close
+  redirect "/products/#{params["product_id"]}"
+end
+
+def create_categories_table
+  c = PGconn.new(:host => "localhost", :dbname => dbname)
+  c.exec %q{
+    CREATE TABLE categories (
+      id SERIAL PRIMARY KEY,
+      name varchar(255)
+    );
+  }
+  c.close
+end
+def drop_categories_table
+  c = PGconn.new(:host => "localhost", :dbname => dbname)
+  c.exec "DROP TABLE categories;"
+  c.close
+end
+
+def create_category_memberships_table
+  c = PGconn.new(:host => "localhost", :dbname => dbname)
+  c.exec %q{
+  CREATE TABLE category_memberships(
+    id SERIAL PRIMARY KEY,
+    product_id integer,
+    category_id integer
+  );
+  }
+  c.close
+end
+
+def drop_category_memberships_table
+  c = PGconn.new(:host => "localhost", :dbname => dbname)
+  c.exec "DROP TABLE category_memberships;"
+  c.close
 end
 
 def create_products_table
